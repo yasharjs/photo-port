be5debdcc4c41233b1c14d12f04121e855f4ac4b
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-24 14:30:33 -0400

    conditionally render categories

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {capitalizeFirstLetter} from '../../utils/helpers'
+import photo from '../../assets/small/commercial/0.jpg'
+
+export default function index() {
+    
+    const currentCategory = {
+        name: 'commercial',
+        description:"Photos of grocery stores, food trucks, and other commercial projects"
+    }
+    return (
+       <section>
+           <h1>
+               {capitalizeFirstLetter(currentCategory.name)}
+           </h1>
+           <p>
+               {currentCategory.description}
+           </p>
+           <div className="flex-row">
+               <img 
+               src={photo}
+               alt="Commercial example"
+               className="img-thumbnail mx-1"
+               />
+           </div>
+       </section>
+    )
+}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,18 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {capitalizeFirstLetter} from "../../utils/helpers"
 
-export default function index() {
-  const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ];
+export default function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
 
-  function categorySelected(name){
-      console.log(`${name} clicked`)
-  }
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  },[currentCategory])
   return (
-    <header>
+    <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
           <span role="img" aria-label="camera">
@@ -32,14 +25,27 @@ export default function index() {
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">About me</a>
+            <a data-testid="about" href="#about">
+              About me
+            </a>
           </li>
-          <li>
+          <li className = "mx-2">
             <span>Contact</span>
           </li>
           {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>{category.name}</span>
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && "navActive"
+              }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
           ))}
         </ul>
